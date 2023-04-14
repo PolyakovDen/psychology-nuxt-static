30f67809824f5deb9e20ada50975ce2c365263e3
Denis Polyakov <denpolyakov63@gmail.com>
2023-04-14 13:22:08 +0300

    add new img v2

--- components/Me.vue
+++ components/Me.vue
@@ -1,39 +1,41 @@
 <template>
-  <v-container id="main" fluid class="pl-5 pr-5 me__container">
-    <v-row no-gutters>
-      <v-col cols="12" lg="5" md="7" sm="12">
-        <div class="me__wrapper">
-          <div class="me" data-aos="fade-right" data-aos-duration="400">
-            <p class="me__description">
-              {{ $t('5') }}
-            </p>
-            <h2 class="me__title">
-              {{ $t('6') }}
-            </h2>
-            <p class="me__annotation mt-10">
-              {{ $t('7') }}
-            </p>
-            <v-dialog
-              v-model="dialog"
-              width="500"
-            >
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  class="consultation__btn pa-10 mt-10"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  {{ $t('8') }}
-                </v-btn>
-              </template>
-              <ConsultationForm />
-            </v-dialog>
+  <div class="me__container">
+    <v-container id="main" fluid class="pl-5 pr-5">
+      <v-row no-gutters>
+        <v-col cols="12" lg="5" md="7" sm="12">
+          <div class="me__wrapper">
+            <div class="me" data-aos="fade-right" data-aos-duration="400">
+              <p class="me__description">
+                {{ $t('5') }}
+              </p>
+              <h2 class="me__title">
+                {{ $t('6') }}
+              </h2>
+              <p class="me__annotation mt-10">
+                {{ $t('7') }}
+              </p>
+              <v-dialog
+                v-model="dialog"
+                width="500"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    class="consultation__btn pa-10 mt-10"
+                    v-bind="attrs"
+                    v-on="on"
+                  >
+                    {{ $t('8') }}
+                  </v-btn>
+                </template>
+                <ConsultationForm />
+              </v-dialog>
+            </div>
           </div>
-          <img src="/main.png" alt="Oleksandr" class="me__image" data-aos="zoom-in-up" data-aos-duration="600">
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+    <img src="/main.png" alt="Oleksandr" class="me__image" data-aos="zoom-in-up" data-aos-duration="600">
+  </div>
 </template>
 
 <script>
@@ -59,7 +61,8 @@ export default {
   .me__image {
     position: absolute;
     height: 100%;
-    right: -15px;
+    right: 0px;
+    top: 0;
     z-index: 1;
   }
   .container {
